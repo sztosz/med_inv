b33227a4b8b4bdb47f84afa7e43b9fe1803bef1e
Prelang Builder <builder@prelang.com>
2015-10-02 21:54:44 +0200

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,66 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151002194604) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contractors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "zip_code"
+    t.string   "city"
+    t.text     "nip"
+    t.integer  "invoice_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contractors", ["invoice_id"], name: "index_contractors_on_invoice_id", using: :btree
+
+  create_table "examination_lists", force: :cascade do |t|
+    t.integer  "month"
+    t.integer  "year"
+    t.integer  "invoice_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "examination_lists", ["invoice_id"], name: "index_examination_lists_on_invoice_id", using: :btree
+
+  create_table "examination_types", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "examination_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "examination_types", ["examination_id"], name: "index_examination_types_on_examination_id", using: :btree
+
+  create_table "examinations", force: :cascade do |t|
+    t.string   "patient"
+    t.datetime "date"
+    t.integer  "examination_list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "examinations", ["examination_list_id"], name: "index_examinations_on_examination_list_id", using: :btree
+
+  create_table "invoices", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
